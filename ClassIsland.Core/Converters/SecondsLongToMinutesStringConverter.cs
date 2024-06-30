d8c68124d77a72725cd8a3c8ff154ec46ecfbbb1
DryIce-cc <dryice_cc@163.com>
2024-06-30 23:55:09 +0800

    feat: 时间点附加信息显示倒计时和小时数

--- ClassIsland.Core/Converters/SecondsLongToMinutesStringConverter.cs
+++ ClassIsland.Core/Converters/SecondsLongToMinutesStringConverter.cs
@@ -7,10 +7,48 @@ public class SecondsLongToMinutesStringConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var v = (long)value;
-        return $"{Math.Ceiling(v*1.0/60):F0}";
+        int v = System.Convert.ToInt32(value) + 59;
+        if (v >= 3600)
+        {
+            return new TimeSpan(0, 0, v).ToString(@"%h\:%m");
+        }
+        else if (v >= 0)
+        {
+            return new TimeSpan(0, 0, v).ToString(@"%m");
+        }
+        else
+        {
+            return 0;
+        }
     }
 
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
+public class SecondsLongToFormatStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        int v = System.Convert.ToInt32(value);
+        if (v >= 3600)
+        {
+            return new TimeSpan(0, 0, v).ToString(@"%h\:mm\:ss");
+        }
+        else if (v >= 60)
+        {
+            return new TimeSpan(0, 0, v).ToString(@"%m\:ss");
+        }
+        else if (v >= 0)
+        {
+            return new TimeSpan(0, 0, v).ToString(@"%s\s");
+        }
+        else
+        {
+            return 0;
+        }
+    }
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         return null;
